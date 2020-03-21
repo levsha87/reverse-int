5c57b11b2f9696155035b69ce6308f75e1eb7dec
Roma Levsha <rm.levsha@gmail.com>
2020-03-22 02:09:12 +0300

    docs: add solution reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+    module.exports = function reverse (n) {
+        let rev = [];
+        n = Math.abs(n);
+        rev = n.toString().split('').reverse();
+        let result = rev.join("");
+        return result;
+        }
 }
